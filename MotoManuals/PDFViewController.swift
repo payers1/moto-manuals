d5196f3685901eaa1ad9219eb4f48819ffa2a7fa
Phill Ayers <phillip.ayers@sharpnotions.com>
2018-09-15 11:17:08 -0400

    fix: Handle rotation in pdf view

--- MotoManuals/PDFViewController.swift
+++ MotoManuals/PDFViewController.swift
@@ -10,15 +10,18 @@ import UIKit
 import PDFKit
 
 class PDFViewController: UIViewController {
+  
   var pdfFileName: String?
   var fileURLs = [URL]()
   var page: Int? = 1
+  var pdfView: PDFView?
   
   override func viewDidLoad() {
     super.viewDidLoad()
     prepareFileURLs()
-    let pdfView = configurePdfView()
-    self.view.addSubview(pdfView)
+    self.pdfView = configurePdfView()
+    self.view?.addSubview(self.pdfView!)
+
   }
   
   func prepareFileURLs() {
@@ -28,15 +31,19 @@ class PDFViewController: UIViewController {
     }
   }
   
-  func configurePdfView() -> UIView {
+  func configurePdfView() -> PDFView {
     let defaultRect = CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height)
     let pdfView = PDFView(frame: defaultRect)
     pdfView.document = PDFDocument(url: fileURLs[0])
-    pdfView.displayMode = .singlePageContinuous
     pdfView.autoScales = true
     pdfView.go(
       to: CGRect(x: 0, y: 40, width: self.view.frame.width, height: self.view.frame.height),
       on: (pdfView.document?.page(at: page! - 1))!)
     return pdfView
+  }
+  
+  override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+    self.pdfView?.frame = CGRect(x: 0, y: 0, width: size.width, height: size.height)
+    self.pdfView?.sizeToFit()
   }
 }
